34d35a87c9a26b95d32abc0e81b07df8ac7c122a
adelliu <lqj679ssn@qq.com>
2018-04-23 20:56:38 -0700

    新建用户时创建文件夹

--- OAuth/views.py
+++ OAuth/views.py
@@ -2,6 +2,7 @@ from Base.decorator import require_get
 from Base.error import Error
 from Base.qtb import get_qtb_user_token
 from Base.response import error_response, response
+from Resource.models import Resource
 from User.models import User
 from User.views import get_token_info
 
@@ -23,6 +24,22 @@ def oauth_qtb_callback(request):
     o_user = ret.body
     if not isinstance(o_user, User):
         return error_response(Error.STRANGE)
+
+    ret = Resource.get_root_folder(o_user)
+    if ret.error is Error.ERROR_GET_ROOT_FOLDER:
+        ret = Resource.get_res_by_id(Resource.ROOT_ID)
+        if ret.error is not Error.OK:
+            return error_response(ret)
+        o_root = ret.body
+
+        ret = Resource.create_folder(
+            o_user.qt_user_app_id,
+            o_user,
+            o_root,
+            '# 齐天用户%s的盒子' % o_user.qt_user_app_id
+        )
+        if ret.error is not Error.OK:
+            return error_response(ret)
 
     ret = o_user.update()
     if ret.error is not Error.OK:
