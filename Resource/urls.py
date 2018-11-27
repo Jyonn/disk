558996119d3da2a6882bbcb858039635850581e8
adelliu <lqj679ssn@qq.com>
2018-11-27 22:05:58 +0800

    调换顺序

--- Resource/urls.py
+++ Resource/urls.py
@@ -8,10 +8,10 @@ from Resource.dev import set_unique_res_key
 from Resource.router import rt_dlpath_callback, rt_cover_callback, rt_res
 
 urlpatterns = [
-    path('<str:res_str_id>/<str:suffix>', rt_res),
+    path('@set_unique_res_key', set_unique_res_key),
+
     path('dlpath/callback', rt_dlpath_callback),
     path('cover/callback', rt_cover_callback),
+    path('<str:res_str_id>/<str:suffix>', rt_res),
     path('<str:res_str_id>', rt_res),
-
-    path('@set_unique_res_key', set_unique_res_key),
 ]
