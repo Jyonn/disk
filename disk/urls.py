56545abc3111a8eb140a148c8f5f7397b538a641
adelliu <lqj679ssn@qq.com>
2019-02-12 17:34:21 +0800

    显示文件名的直链模式

--- disk/urls.py
+++ disk/urls.py
@@ -13,14 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.http import HttpResponseRedirect
 from django.urls import path, include
 
 from Resource.router import rt_direct_link
-
-
-# def page_not_found(request):
-#     return HttpResponseRedirect('https://github.com/lqj679ssn/disk')
 
 
 urlpatterns = [
