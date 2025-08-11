f92f6428c466cbc6ef69d7479cc629853e113be6
jyonnliu <i@6-79.cn>
2025-08-11 11:52:36 +0800

    update jwterror

--- Base/common.py
+++ Base/common.py
@@ -16,7 +16,7 @@ if PROJ_INIT:
 else:
     QITIAN_APP_ID = Config.get_value_by_key(CI.QITIAN_APP_ID)
     QITIAN_APP_SECRET = Config.get_value_by_key(CI.QITIAN_APP_SECRET)
-    QITIAN_HOST = Config.get_config_by_key(CI.QITIAN_HOST)
+    QITIAN_HOST = Config.get_value_by_key(CI.QITIAN_HOST)
 
     SECRET_KEY = Config.get_value_by_key(CI.PROJECT_SECRET_KEY)
     JWT_ENCODE_ALGO = Config.get_value_by_key(CI.JWT_ENCODE_ALGO)
