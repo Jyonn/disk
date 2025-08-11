727e3c84354801a0b20e0a7dfe70aebf0e18fb8c
jyonnliu <i@6-79.cn>
2025-08-11 09:33:26 +0800

    enable proj_init

--- Base/common.py
+++ Base/common.py
@@ -5,22 +5,25 @@ from QitianSDK import QitianManager
 from Config.models import Config, CI
 from disk.settings import PROJ_INIT
 
-DEBUG = True
-
-
-QITIAN_APP_ID = Config.get_value_by_key(CI.QITIAN_APP_ID)
-QITIAN_APP_SECRET = Config.get_value_by_key(CI.QITIAN_APP_SECRET)
-QITIAN_HOST = Config.get_config_by_key(CI.QITIAN_HOST)
-
-SECRET_KEY = Config.get_value_by_key(CI.PROJECT_SECRET_KEY)
-JWT_ENCODE_ALGO = Config.get_value_by_key(CI.JWT_ENCODE_ALGO)
-
-HOST = Config.get_value_by_key(CI.HOST)
-ADMIN_QITIAN = Config.get_value_by_key(CI.ADMIN_QITIAN)
 
 if PROJ_INIT:
     MAX_IMAGE_SIZE = MAX_FILE_SIZE = 0
+
+    QITIAN_APP_ID = QITIAN_APP_SECRET = QITIAN_HOST = None
+    SECRET_KEY = JWT_ENCODE_ALGO = None
+
+    HOST = ADMIN_QITIAN = None
 else:
+    QITIAN_APP_ID = Config.get_value_by_key(CI.QITIAN_APP_ID)
+    QITIAN_APP_SECRET = Config.get_value_by_key(CI.QITIAN_APP_SECRET)
+    QITIAN_HOST = Config.get_config_by_key(CI.QITIAN_HOST)
+
+    SECRET_KEY = Config.get_value_by_key(CI.PROJECT_SECRET_KEY)
+    JWT_ENCODE_ALGO = Config.get_value_by_key(CI.JWT_ENCODE_ALGO)
+
+    HOST = Config.get_value_by_key(CI.HOST)
+    ADMIN_QITIAN = Config.get_value_by_key(CI.ADMIN_QITIAN)
+
     MAX_IMAGE_SIZE = int(Config.get_value_by_key(CI.MAX_IMAGE_SIZE))
     MAX_FILE_SIZE = int(Config.get_value_by_key(CI.MAX_FILE_SIZE))
 
